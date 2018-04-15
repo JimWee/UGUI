cf03ca01b25da9840e727264da41b1594242f67e
WeiJianhui <18817551783@163.com>
2018-04-15 16:38:32 +0800

    UISpriteAtlas

--- Assets/UGUI/Scripts/GameManger.cs
+++ Assets/UGUI/Scripts/GameManger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class GameManger : MonoBehaviour {
 
@@ -18,7 +19,12 @@ public class GameManger : MonoBehaviour {
         var d = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "at"));
         var ab = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "ui"));
         var prefab = ab.LoadAsset<GameObject>("Image");
-        GameObject.Instantiate(prefab, transform);
+        var image = GameObject.Instantiate<GameObject>(prefab, transform);
+        image = GameObject.Instantiate<GameObject>(prefab, transform);
+        image.transform.localPosition = new Vector3(100f, 0f, 0f);
+        var s = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "res"));
+        var usa = s.LoadAsset<UISpriteAtlas>("Bn");
+        image.GetComponent<Image>().sprite = usa.GetSprite("UI_UP");
         //prefab = ab.LoadAsset<GameObject>("Image2");
         //GameObject.Instantiate(prefab, transform);
     }
---
+++ Assets/UGUI/Scripts/UISpriteAtlas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UISpriteAtlas", menuName = "UI Sprite Atlas", order = 1)]
+class UISpriteAtlas : ScriptableObject
+{
+    public Sprite[] Sprites;
+
+    public int spriteCount
+    {
+        get
+        {
+            if (Sprites != null)
+            {
+                return Sprites.Length;
+            }
+            return 0;
+        }
+    }
+
+    public Sprite GetSprite(string name)
+    {
+        if (Sprites != null)
+        {
+            foreach (var sprite in Sprites)
+            {
+                if (sprite != null && sprite.name == name)
+                {
+                    return sprite;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Sprite[] GetSprites()
+    {
+        return Sprites;
+    }
+}
