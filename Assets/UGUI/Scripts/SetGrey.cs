9b515c857735e47a0415ef1891242b9035308d50
WeiJianhui <18817551783@163.com>
2018-04-17 18:39:00 +0800

    grey

---
+++ Assets/UGUI/Scripts/SetGrey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGrey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var renderer = GetComponent<Renderer>();
+        var prop = new MaterialPropertyBlock();
+        prop.SetFloat("_Grey", 1);
+        renderer.SetPropertyBlock(prop);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
