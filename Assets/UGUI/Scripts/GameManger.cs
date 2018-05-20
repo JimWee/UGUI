c7b639a0d648d49288664baa2334f129432a916e
WeiJianhui <18817551783@163.com>
2018-05-20 18:10:12 +0800

    事件检测

--- Assets/UGUI/Scripts/GameManger.cs
+++ Assets/UGUI/Scripts/GameManger.cs
@@ -6,31 +6,47 @@ using UnityEngine.UI;
 
 public class GameManger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        var m = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AB"));
-        AssetBundleManifest am = m.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-        var dp = am.GetAllDependencies("ui");
-        Debug.LogWarning("Test");
-        foreach (var dd in dp)
+    public Button button;
+
+    // Use this for initialization
+    //void Start () {
+    //       var m = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AB"));
+    //       AssetBundleManifest am = m.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+    //       var dp = am.GetAllDependencies("ui");
+    //       Debug.LogWarning("Test");
+    //       foreach (var dd in dp)
+    //       {
+    //           Debug.LogWarning(dd);
+    //       }
+    //       var d = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "at"));
+    //       var ab = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "ui"));
+    //       var prefab = ab.LoadAsset<GameObject>("Image");
+    //       var image = GameObject.Instantiate<GameObject>(prefab, transform);
+    //       image = GameObject.Instantiate<GameObject>(prefab, transform);
+    //       image.transform.localPosition = new Vector3(100f, 0f, 0f);
+    //       var s = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "res"));
+    //       var usa = s.LoadAsset<UISpriteAtlas>("Bn");
+    //       image.GetComponent<Image>().sprite = usa.GetSprite("UI_UP");
+    //       //prefab = ab.LoadAsset<GameObject>("Image2");
+    //       //GameObject.Instantiate(prefab, transform);
+    //   }
+
+    private void Start()
+    {
+        button.onClick.AddListener(delegate ()
         {
-            Debug.LogWarning(dd);
-        }
-        var d = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "at"));
-        var ab = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "ui"));
-        var prefab = ab.LoadAsset<GameObject>("Image");
-        var image = GameObject.Instantiate<GameObject>(prefab, transform);
-        image = GameObject.Instantiate<GameObject>(prefab, transform);
-        image.transform.localPosition = new Vector3(100f, 0f, 0f);
-        var s = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "res"));
-        var usa = s.LoadAsset<UISpriteAtlas>("Bn");
-        image.GetComponent<Image>().sprite = usa.GetSprite("UI_UP");
-        //prefab = ab.LoadAsset<GameObject>("Image2");
-        //GameObject.Instantiate(prefab, transform);
+            Debug.Log("AAAA");
+        });
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
+
+    public void OnButtonClicked(string s)
+    {
+        Debug.Log("On Button Clicked" + s);
+    }
+
 }
---
+++ Assets/UGUI/Scripts/ThreeDEventTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ThreeDEventTest : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Color HighlightedColor = Color.red;
+    public Color NormalColor = Color.white;
+    public Color PressedColor = Color.green;
+
+    private Material m;
+
+    void Start () {
+        var renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            m = renderer.material;
+            m.color = NormalColor;
+        }
+	}
+	
+	void Update () {
+		
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(m != null)
+        {
+            m.color = HighlightedColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (m != null)
+        {
+            m.color = NormalColor;
+        }
+    }
+}
