d3d922b46deecf1965cbf3d938da8ebd3e8b380f
WeiJianhui <18817551783@163.com>
2018-06-12 20:41:14 +0800

    DOTween

---
+++ Assets/UGUI/Scripts/UIPressScale.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIPressScale : MonoBehaviour {
+
+    public int flag = 0;
+
+    void Awake()
+    {
+        DOTween.Init(true, true, LogBehaviour.ErrorsOnly);
+    }
+
+    // Use this for initialization
+    void Start () {
+        switch (flag)
+        {
+            case 1:
+                DOTween.To(() => transform.localPosition, x => transform.localPosition = x, new Vector3(100, 100, 100), 2);
+                break;
+            case 2:
+                transform.DOLocalMove(new Vector3(100, 100, 100), 2).From();
+                break;
+            case 3:
+                var s = DOTween.Sequence();
+                s.Append(transform.DOLocalMove(new Vector3(100, 100, 100), 2));
+                s.Append(transform.DOLocalRotate(new Vector3(0, 0, 90), 2));
+                s.PrependCallback(() => Debug.Log("callback"));
+                s.PrependInterval(1f);
+                break;
+            default:
+                break;
+        }             
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
