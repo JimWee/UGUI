41a704c1f457b45d58464379d2bae40420a80a97
WeiJianhui <18817551783@163.com>
2018-06-13 21:10:55 +0800

    DOTween

--- Assets/UGUI/Scripts/UIPressScale.cs
+++ Assets/UGUI/Scripts/UIPressScale.cs
@@ -2,40 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.EventSystems;
+using System;
 
-public class UIPressScale : MonoBehaviour {
+public class UIPressScale : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Vector3 pressed = new Vector3(1.1f, 1.1f, 1.1f);
+    public float duration = 0.2f;
 
-    public int flag = 0;
+    private Vector3 mScale;
 
-    void Awake()
+    private void Awake()
     {
-        DOTween.Init(true, true, LogBehaviour.ErrorsOnly);
+        mScale = transform.localScale;
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        transform.DOScale(pressed, duration);
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+        transform.DOScale(mScale, duration);
+
+    }
+
+    //public int flag = 0;
+
+    //void Awake()
+    //{
+    //    DOTween.Init(true, true, LogBehaviour.ErrorsOnly);
+    //}
+
     // Use this for initialization
-    void Start () {
-        switch (flag)
-        {
-            case 1:
-                DOTween.To(() => transform.localPosition, x => transform.localPosition = x, new Vector3(100, 100, 100), 2);
-                break;
-            case 2:
-                transform.DOLocalMove(new Vector3(100, 100, 100), 2).From();
-                break;
-            case 3:
-                var s = DOTween.Sequence();
-                s.Append(transform.DOLocalMove(new Vector3(100, 100, 100), 2));
-                s.Append(transform.DOLocalRotate(new Vector3(0, 0, 90), 2));
-                s.PrependCallback(() => Debug.Log("callback"));
-                s.PrependInterval(1f);
-                break;
-            default:
-                break;
-        }             
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //   void Start () {
+    //       switch (flag)
+    //       {
+    //           case 1:
+    //               DOTween.To(() => transform.localPosition, x => transform.localPosition = x, new Vector3(100, 100, 100), 2);
+    //               break;
+    //           case 2:
+    //               transform.DOLocalMove(new Vector3(100, 100, 100), 2).From();
+    //               break;
+    //           case 3:
+    //               var s = DOTween.Sequence();
+    //               s.Append(transform.DOLocalMove(new Vector3(100, 100, 100), 2));
+    //               s.Append(transform.DOLocalRotate(new Vector3(0, 0, 90), 2));
+    //               s.PrependCallback(() => Debug.Log("callback"));
+    //               s.PrependInterval(1f);
+    //               break;
+    //           default:
+    //               break;
+    //       }             
+    //}
 }
