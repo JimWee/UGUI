b2600776bd5b143268460e7fd17547f0e963dcf2
WeiJianhui <18817551783@163.com>
2018-08-30 20:03:50 +0800

    NGUI vs UGUI

--- Assets/UGUI/Scripts/GameManger.cs
+++ Assets/UGUI/Scripts/GameManger.cs
@@ -9,35 +9,36 @@ public class GameManger : MonoBehaviour {
     public Button button;
 
     // Use this for initialization
-    //void Start () {
-    //       var m = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AB"));
-    //       AssetBundleManifest am = m.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-    //       var dp = am.GetAllDependencies("ui");
-    //       Debug.LogWarning("Test");
-    //       foreach (var dd in dp)
-    //       {
-    //           Debug.LogWarning(dd);
-    //       }
-    //       var d = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "at"));
-    //       var ab = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "ui"));
-    //       var prefab = ab.LoadAsset<GameObject>("Image");
-    //       var image = GameObject.Instantiate<GameObject>(prefab, transform);
-    //       image = GameObject.Instantiate<GameObject>(prefab, transform);
-    //       image.transform.localPosition = new Vector3(100f, 0f, 0f);
-    //       var s = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "res"));
-    //       var usa = s.LoadAsset<UISpriteAtlas>("Bn");
-    //       image.GetComponent<Image>().sprite = usa.GetSprite("UI_UP");
-    //       //prefab = ab.LoadAsset<GameObject>("Image2");
-    //       //GameObject.Instantiate(prefab, transform);
-    //   }
-
-    private void Start()
+    void Start()
     {
-        button.onClick.AddListener(delegate ()
+        var m = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AB"));
+        AssetBundleManifest am = m.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        var dp = am.GetAllDependencies("prefabs");
+        Debug.LogWarning("Test");
+        foreach (var dd in dp)
         {
-            Debug.Log("AAAA");
-        });
+            Debug.LogWarning(dd);
+        }
+        var d = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "sprite atlas"));
+        var ab = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "prefabs"));
+        var prefab = ab.LoadAsset<GameObject>("UGUI Sprite Atlas");
+        var image = GameObject.Instantiate<GameObject>(prefab, transform);
+        //image = GameObject.Instantiate<GameObject>(prefab, transform);
+        //image.transform.localPosition = new Vector3(100f, 0f, 0f);
+        //var s = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "res"));
+        //var usa = s.LoadAsset<UISpriteAtlas>("Bn");
+        //image.GetComponent<Image>().sprite = usa.GetSprite("UI_UP");
+        //prefab = ab.LoadAsset<GameObject>("Image2");
+        //GameObject.Instantiate(prefab, transform);
     }
+
+    //private void Start()
+    //{
+    //    button.onClick.AddListener(delegate ()
+    //    {
+    //        Debug.Log("AAAA");
+    //    });
+    //}
 
     // Update is called once per frame
     void Update () {
---
+++ Assets/UI/Scripts/SetGrey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SetGrey : MonoBehaviour {
+
+    public Material GreyMaterial;
+    public bool Grey = false;
+
+    private Image mImage;
+    private Material mDefaultMaterial;
+
+	void Start () {
+        mImage = GetComponent<Image>();
+        mDefaultMaterial = mImage.material;
+        UpdateGrey();
+    }
+	
+
+    private void OnValidate()
+    {
+        UpdateGrey();
+    }
+
+    private void UpdateGrey()
+    {
+        if (mImage != null)
+        {
+            mImage.material = Grey ? GreyMaterial : mDefaultMaterial;
+        }
+    }
+}
